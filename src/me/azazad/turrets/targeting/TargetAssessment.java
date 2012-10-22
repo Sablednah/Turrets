0c673dab1730212859dbd3f11eea416a82924d86
wristdirect <wristdirect@gmail.com>
2012-10-22 15:10:33 -0400

    blah

--- src/me/azazad/turrets/targeting/TargetAssessment.java
+++
@@ -1,5 +0,0 @@
-package me.azazad.turrets.targeting;
-
-public enum TargetAssessment{
-    HOSTILE,NOT_HOSTILE,MEH
-}
--- src/me/azazad/turrets/targeting/TargetAssessor.java
+++
@@ -1,7 +0,0 @@
-package me.azazad.turrets.targeting;
-
-import org.bukkit.entity.LivingEntity;
-
-public interface TargetAssessor{
-    public TargetAssessment assessMob(LivingEntity mob);
-}
